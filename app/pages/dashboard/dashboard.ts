41192ac30581a7ee18d1cf4069f172dfd5ee665f
Harsh Kumar <139384568+Harshu70@users.noreply.github.com>
2026-02-09 01:18:29 +0530

    Add files via upload
    change chart

--- app/pages/dashboard/dashboard.ts
+++ app/pages/dashboard/dashboard.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Transaction } from '../../services/transaction';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-// import { HttpClient } from '@angular/common/http';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
@@ -13,6 +13,10 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
 })
 export class Dashboard implements OnInit {
+
+  @ViewChild('myChart') chartCanvas!: ElementRef;
+  chart: any;
+
   stats: any = { income: 0, expense: 0, balance: 0 };
   transactions: any[] = [];
   transactionData = {
@@ -31,10 +35,31 @@ export class Dashboard implements OnInit {
   loadData() {
     this.transactionService.getStats().subscribe((res) => {
       this.stats = res;
+      this.updateChart();
     });
     this.transactionService.getAll().subscribe((res) => {
       this.transactions = res;
     });
+  }
+  updateChart(){
+    if(this.chart) this.chart.destroy();
+    this.chart = new Chart(this.chartCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['Income', 'Expense'],
+        datasets: [{
+          data: [this.stats.income, this.stats.expense],
+          backgroundColor: ['#10B981', '#EF4444'],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        responsive: true, 
+        plugins: {
+          legend: {position: 'bottom'}
+        }
+      }
+    })
   }
   onSubmit() {
     if (!this.transactionData.amount || !this.transactionData.description) {
